48cf4db687c45472ae77ae523dd7418200160dd6
Hendrixer <willscottmoss@gmail.com>
2013-11-28 13:41:29 -0800

    more tests

--- client/views/SongQueueEntryView.js
+++ client/views/SongQueueEntryView.js
@@ -3,19 +3,5 @@ window.MyTunes = window.MyTunes || {};
 window.MyTunes.Views = window.MyTunes.Views || {};
 
 MyTunes.Views.SongQueueEntryView = Backbone.View.extend({
-
-  tagName: 'li',
-
-  template: _.template('(<%= artist %>)<%= title %>'),
-
-  events: {
-    'click': function() {
-      this.model.enqueue();
-    }
-  },
-
-  render: function() {
-    return this.$el.html(this.template(this.model.attributes));
-  }
-
+  
 });
--- client/views/SongQueueView.js
+++ client/views/SongQueueView.js
@@ -3,20 +3,8 @@ window.MyTunes = window.MyTunes || {};
 window.MyTunes.Views = window.MyTunes.Views || {};
 
 MyTunes.Views.SongQueueView = Backbone.View.extend({
-
-  tagName: 'ul',
-
+  
   initialize: function() {
-    this.render();
-  },
 
-  render: function() {
-    this.$el.children.detach();
-
-    this.$('<h1>queue</h1>').append(
-      this.collection.map(function(song) {
-        return new MyTunes.Views.SongQueueEntryView({model: song}).render();
-      })
-    );
   }
 });
