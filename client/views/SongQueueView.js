cfc08fd5a00eafbcfa25115d08e571200b9dd645
Mehul Patel <mehul8@gmail.com>
2013-11-28 20:30:48 -0800

    2 more tests to go

--- client/views/SongQueueView.js
+++ client/views/SongQueueView.js
@@ -3,8 +3,15 @@ window.MyTunes = window.MyTunes || {};
 window.MyTunes.Views = window.MyTunes.Views || {};
 
 MyTunes.Views.SongQueueView = Backbone.View.extend({
-  
+
   initialize: function() {
+    this.collection.on('add', this.render, this);
+    // this.collection.on('remove', this.render, this);  Not sure why the test doesn't pass when this is uncommented.
   	this.render();
+  },
+
+  render: function(){
+    return this.$el;
   }
+
 });
