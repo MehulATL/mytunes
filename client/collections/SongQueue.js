959dc267e3c282c8c5aa3df3654156d5f20f1b18
Mehul Patel <mehul8@gmail.com>
2013-11-28 17:23:50 -0800

    implemented removeFirst();

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -5,9 +5,14 @@ window.MyTunes.Collections = window.MyTunes.Collections || {};
 MyTunes.Collections.SongQueue = MyTunes.Collections.Songs.extend({
 
   initialize: function(){
+    this.on('ended', this.removeFirst, this);
   },
 
   playFirst: function(song) {
     this.at(0).play();
+  },
+
+  removeFirst: function(){
+    this.remove(this.at(0));
   }
 });
